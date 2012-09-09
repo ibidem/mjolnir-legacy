4855b438a8cb4c1dab4c50ed6e4ecbd9e62ef373
srcspider <source.spider@gmail.com>
2012-09-10 02:12:15 +0300

    updated honeypot

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -3,12 +3,13 @@
 // This is an IDE honeypot. It tells IDEs the class hirarchy, but otherwise has
 // no effect on your application. :)
 
-// HowTo: order honeypot -n 'ibidem\legacy'
+// HowTo: order honeypot -n 'mjolnir\legacy'
 
 class Cache_APC extends \mjolnir\legacy\Cache_APC { /** @return \mjolnir\legacy\Cache_APC */ static function instance() { return parent::instance(); } }
 class Cache_File extends \mjolnir\legacy\Cache_File { /** @return \mjolnir\legacy\Cache_File */ static function instance() { return parent::instance(); } }
 class Cache_Memcached extends \mjolnir\legacy\Cache_Memcached { /** @return \mjolnir\legacy\Cache_Memcached */ static function instance() { return parent::instance(); } }
 class Cache extends \mjolnir\legacy\Cache { /** @return \mjolnir\legacy\Cache */ static function instance() { return parent::instance(); } }
+class Controller_HTTP extends \mjolnir\legacy\Controller_HTTP { /** @return \mjolnir\legacy\Controller_HTTP */ static function instance() { return parent::instance(); } }
 class Event extends \mjolnir\legacy\Event { /** @return \mjolnir\legacy\Event */ static function instance() { return parent::instance(); } }
 class Layer_HTML extends \mjolnir\legacy\Layer_HTML { /** @return \mjolnir\legacy\Layer_HTML */ static function instance() { return parent::instance(); } }
 class Layer_HTTP extends \mjolnir\legacy\Layer_HTTP { /** @return \mjolnir\legacy\Layer_HTTP */ static function instance() { return parent::instance(); } }
@@ -20,5 +21,5 @@ class Migration extends \mjolnir\legacy\Migration { /** @return \mjolnir\legacy\
 class Model_Factory extends \mjolnir\legacy\Model_Factory {}
 class Model_Instantiatable extends \mjolnir\legacy\Model_Instantiatable { /** @return \mjolnir\legacy\Model_Instantiatable */ static function instance($id = null) { return parent::instance($id); } }
 class Model_SQL_Factory extends \mjolnir\legacy\Model_SQL_Factory {}
-class Relay extends \mjolnir\legacy\Relay { /** @return \mjolnir\legacy\Relay */ static function instance() { return parent::instance(); } }
+class Relay extends \mjolnir\legacy\Relay {}
 class Task_Migrate extends \mjolnir\legacy\Task_Migrate { /** @return \mjolnir\legacy\Task_Migrate */ static function instance($encoded_task = null) { return parent::instance($encoded_task); } }
