fd1adf38e075851723133c25d3981836a5228029
srcspider <source.spider@gmail.com>
2013-10-01 12:21:47 +0300

    moved not_empty and valid_number to legacy
    The rules are now not-empty and valid-number.

---
+++ +App/config/mjolnir/validator.php
@@ -0,0 +1,9 @@
+<?php return array
+	(
+		'errors' => array
+			(
+				'not_empty' => \app\Lang::term('Field is required.'),
+				'valid_number' => \app\Lang::term('Field must be valid number.'),
+			),
+
+	); # config
